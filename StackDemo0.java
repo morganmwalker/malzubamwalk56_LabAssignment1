8892ec1b3a2cd499f64cfd18e08f1364901e7721
mariam-alzu3 <90797083+mariam-alzu3@users.noreply.github.com>
2021-10-10 19:23:08 -0400

    Update StackDemo0.java

--- StackDemo0.java
+++ StackDemo0.java
@@ -2,10 +2,10 @@ package LA1Q2;
 
 public class StackDemo0 {
     public static void main(String[] args) {
-        header();
+        header();   //calling header method
 
         // create an empty String stack
-        MyStack <String> newStack = new MyStack<>(0);
+        MyStack<String> newStack = new MyStack<>(0);
 
         // print the stack content with the help of the toString() method
         System.out.println(newStack.toString());
@@ -15,7 +15,7 @@ public class StackDemo0 {
 
         //pushing each element of the student array to the stack using push() method
         System.out.println("Pushing the String values to the stack....");
-        for (int i = 0; i < student.length ; i++) {
+        for (int i = 0; i < student.length; i++) {
             newStack.push(student[i]);
         }
         //printing the stack that now contains the names and student numbers
@@ -26,7 +26,7 @@ public class StackDemo0 {
 
         //search the stack for the first student number
         System.out.println("Searching for 251183118...");
-        System.out.printf("The item has been found in distance %s with reference to the top\n", newStack.search("251183118"));
+        System.out.println("The item has been found in distance " + newStack.search("251183118") + " with reference to the top");
 
         // store the distance metric as a String variable
         String studentNumber = student[(newStack.search("251183118"))];
@@ -35,24 +35,26 @@ public class StackDemo0 {
         int sn = (Integer.parseInt(studentNumber));
 
         // find last digit
-        int snLast = sn%10;
+        int snLast = sn % 10;
 
         // find first digit
         int snFirst = 0;
-        while(sn!=0) {
-            snFirst = sn%10;
+        while(sn != 0) {
+            snFirst = sn % 10;
             sn /= 10;
         }
-        System.out.println("The first number in the student ID is " + snFirst);
 
+        System.out.println("The first number in the student ID is " + snFirst);
         System.out.println("The last number in the student ID is " + snLast);
 
         // print the average of the first and last digit
         System.out.println("The average of these two numbers is: " + ((double)snFirst + (double)snLast)/2);
 
-        footer(newStack);
+        footer(newStack);   //calling the footer method
     }
 
+
+    //header method
     public static void header() {
         System.out.println("*************************");
         System.out.println("Names: Morgan Walker and Mariam Al-Zubaidi\nStudent Number: 251183118 and 251077904");
@@ -60,18 +62,28 @@ public class StackDemo0 {
         System.out.println("*************************\n");
     }
 
-    public static <E> void footer(E v) {
-        MyStack<String> finalStack = (MyStack<String>) v;
+    //footer method
+    public static void footer(MyStack finalStack) {
+        //the parameter v is the newStack that contains all the values
+        //casting it to be able to use it inside the footer() method
+        //MyStack<String> finalStack = (MyStack<String>) v;
+        //Team Member 1 info:
         System.out.println("\nTeam Member 1 info.....");
+        //popping the stack 3 times
         System.out.println("    First Name: " + finalStack.pop());
         System.out.println("    Last Name: " + finalStack.pop());
         System.out.println("    Student Number: " + finalStack.pop());
+
+        //Team Member 2 info:
         System.out.println("Team Member 2 info.....");
+        //popping the stack 3 times
         System.out.println("    First Name: " + finalStack.pop());
         System.out.println("    Last Name: " + finalStack.pop());
         System.out.println("    Student Number: " + finalStack.pop());
         System.out.println("Here is the status of the Stack...");
-        System.out.println(v.toString());
+        System.out.println(finalStack.toString() );
         System.out.println("Goodbye!");
     }
+
 }
+

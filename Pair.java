5d1fe4bb65f6c7f45b1bef968a16fd581b7554ef
mariam-alzu3 <90797083+mariam-alzu3@users.noreply.github.com>
2021-10-10 19:24:56 -0400

    Update Pair.java

--- Pair.java
+++ Pair.java
@@ -8,10 +8,11 @@ public class Pair<Y, N> {
     public static void main(String[] args) {
         header(); // print header
 
+        //ArrayLists to store the names and years of the students
         ArrayList<Integer> studentYear = new ArrayList(Arrays.asList(2, 3, 4, 3, 2, 2));
-
         ArrayList<String> studentName = new ArrayList(Arrays.asList("Harry", "Lavender", "Ron", "Hermione", "Luna", "Vincent"));
 
+        //an array that with the length of the studentYear ArrayList
         Pair[] yn = new Pair[studentYear.size()];
 
         // populate array with student leader year and name
@@ -19,21 +20,23 @@ public class Pair<Y, N> {
             yn[i] = new Pair(studentYear.get(i), studentName.get(i));
         }
 
+        //user input to get the year
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Let’s check out the leaders from different year of program.");
-
+        // loop to ask the user what year they want the names from
         char cont = 'y';
         do {
             System.out.print("From which academic year would you like to list the names of the leaders: ");
             int year = 0;
 
+            //try-catch to check if the user doesnt input an int
             try {
                 year = scan.nextInt();
             } catch (Exception e) {
                 scan.nextLine();
             }
 
+            //try-catch to check if the user inputs years from 2-4
             while (year != 2 && year != 3 && year != 4) {
                 System.out.print("Invalid Entry! Enter a valid number between 2 and 4: ");
 
@@ -63,26 +66,34 @@ public class Pair<Y, N> {
             System.out.print("Do you want to continue? (y/n): ");
             cont = scan.next().charAt(0);
 
+            //to check if the entry is anything other than y or n it will print Invalid Entry!
             while (((cont != 'n') && (cont != 'y')) && ((cont != 'N') && (cont != 'Y')) ) {
                 System.out.print("Invalid Entry! Enter y or n: ");
                 cont = scan.next().charAt(0);
             }
 
+            //the code will loop if the user enters y
         } while (cont == 'y' || cont == 'Y');
 
+        //print statement for the reps
         System.out.println("\nHere are the students reps: ");
+        //calling the getRep method to get the names of the reps
         getRep_MariamMorgan(yn);
 
         footer(); // print footer
     }
 
+    //data fields
     private Y key;
     private N value;
 
+    //constructor with parameters
     public Pair(Y key, N value) {
         this.key = key;
         this.value = value;
     }
+
+    //set methods for key and value
 
     public void setKey(Y key) {
         this.key = key;
@@ -92,6 +103,8 @@ public class Pair<Y, N> {
         this.value = value;
     }
 
+    //get methods for key and value
+
     public Y getKey() {
         return key;
     }
@@ -100,10 +113,13 @@ public class Pair<Y, N> {
         return value;
     }
 
-    // question (d)
+    // method that will check the Pair[] array and add the first number and corresponding name to a new array
+    // and if that number exists again it will ignore it
     public static <E> ArrayList<E> getRep_MariamMorgan(Pair[] pa) {
+        //creating a new ArrayList that is empty
         ArrayList<String> repArray = new ArrayList<String>();
 
+        //...
         for (int i = 0; i < pa.length; i++) {
             if (repArray.contains(pa[i].getKey().toString()) == false) {
                 repArray.add(pa[i].getKey().toString());
@@ -111,16 +127,18 @@ public class Pair<Y, N> {
             }
         }
 
+        //for loop to print the years by starting at repArray[0] and incrementing it 2 so it prints repArray[2]
+        //then to print the names it starts at 1 and increments by 2; to print repArray[3]
         for (int i = 0; i < repArray.size(); i += 2) {
             System.out.print(repArray.get(i) + " - ");
             System.out.println(repArray.get(i + 1));
         }
 
-        //System.out.println(repArray);
-
+        //returns new array that contains only the reps
         return (ArrayList<E>) repArray;
     }
 
+    //header method
     public static void header() {
         System.out.println("*****************************");
         System.out.println("Name: Morgan Walker and Mariam Al-Zubaidi\nStudent Number: 251183118 and 251077904");
@@ -128,6 +146,7 @@ public class Pair<Y, N> {
         System.out.println("*****************************\n");
     }
 
+    //footer method
     public static void footer() {
         System.out.println("\n*****************************");
         System.out.println("This is timeOfDay on theDate");
